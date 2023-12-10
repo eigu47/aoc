aa66690cba78f2d8b79594bac7cae56802a34f69
Eiguchi Pablo <pablo.eiguchi@gmail.com>
2023-12-11 01:16:07 +0900

    other version day3-1

--- 2023/day3.ts
+++ 2023/day3.ts
@@ -2,38 +2,25 @@ import fs from "fs";
 
 const input = (fs.readFileSync("day3", "utf8") as string)
   .split("\n")
-  .map((line, idx, arr) => {
-    line = line.trim();
+  .map((line, idx, arr) =>
+    [...line.trim().matchAll(/(\d+)/g)].map((num) => {
+      const top =
+        arr[idx - 1]
+          ?.substring(num.index! - 1, num.index! + num[0].length + 1)
+          .trim() ?? "";
+      const lft = line[num.index! - 1]?.trim() ?? "";
+      const rgt = line[num.index! + num[0].length]?.trim() ?? "";
+      const btm =
+        arr[idx + 1]
+          ?.substring(num.index! - 1, num.index! + num[0].length + 1)
+          .trim() ?? "";
 
-    const nums: number[] = [];
-
-    for (let i = 0; i < line.length; i++) {
-      if (!isNaN(+line[i])) {
-        let num = line[i];
-
-        while (++i < line.length && !isNaN(+line[i])) {
-          num += line[i];
-        }
-
-        const top =
-          arr[idx - 1]?.substring(i - num.length - 1, i + 1).trim() ?? "";
-        const lft = line[i - num.length - 1] ?? "";
-        const rgt = line[i] ?? "";
-        const btm =
-          arr[idx + 1]?.substring(i - num.length - 1, i + 1).trim() ?? "";
-
-        const adjacent = top + lft + rgt + btm;
-
-        if (/[^0-9.]/.test(adjacent)) {
-          console.log({ num, adjacent });
-          nums.push(+num);
-        }
+      if (/[^0-9.]/.test(top + lft + rgt + btm)) {
+        return +num[0];
       }
-    }
-
-    return nums;
-  })
+    })
+  )
   .flat()
-  .reduce((acc, cur) => acc + cur, 0);
+  .reduce((acc, cur) => acc! + (cur ?? 0), 0);
 
 console.log(input);
