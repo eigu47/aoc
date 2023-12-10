883322574d5080510a98c72cd5bac203ae9d2f66
Eiguchi Pablo <pablo.eiguchi@gmail.com>
2023-12-10 20:51:47 +0900

    day4-2

--- 2023/day4.ts
+++ 2023/day4.ts
@@ -2,19 +2,26 @@ import fs from "fs";
 
 const input = (fs.readFileSync("day4", "utf8") as string)
   .split("\n")
-  .map((line) => [...line.split(":")[1].split("|"), "1"])
-  .reduce((acc, cur) => {
-    const winners = cur[0].match(/\d+/g);
-    const card = cur[1].match(/\d+/g);
-
-    const wins = winners?.reduce((acc, cur) => {
-      if (card?.some((c) => c == cur)) {
-        return !acc ? 1 : (acc *= 2);
+  .map((line) => {
+    const [winners, card] = line
+      .split(":")[1]
+      .split("|")
+      .map((str) => str.match(/\d+/g));
+    return { winners, card, count: 1 };
+  })
+  .reduce((acc, { winners, card, count }, idx, arr) => {
+    let wins = 0;
+    winners?.forEach((winner) => {
+      if (card?.includes(winner)) {
+        wins++;
+        const next = arr[idx + wins];
+        if (next) {
+          next.count += count;
+        }
       }
-      return acc;
-    }, 0);
+    });
 
-    return acc + (wins ?? 0);
+    return acc + count;
   }, 0);
 
 console.log(input);
