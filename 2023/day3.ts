c156898d78b7f73c04ca3720381837c4d204093e
Eiguchi Pablo <pablo.eiguchi@gmail.com>
2023-12-11 13:09:46 +0900

    day3-2 (not working)

--- 2023/day3.ts
+++ 2023/day3.ts
@@ -4,23 +4,42 @@ const input = (fs.readFileSync("day3", "utf8") as string)
   .split("\n")
   .map((line, idx, arr) =>
     [...line.trim().matchAll(/(\d+)/g)].map((num) => {
-      const top =
-        arr[idx - 1]
-          ?.substring(num.index! - 1, num.index! + num[0].length + 1)
-          .trim() ?? "";
-      const lft = line[num.index! - 1]?.trim() ?? "";
-      const rgt = line[num.index! + num[0].length]?.trim() ?? "";
-      const btm =
-        arr[idx + 1]
-          ?.substring(num.index! - 1, num.index! + num[0].length + 1)
-          .trim() ?? "";
+      if (num.index === undefined) return;
+      const adjacent: [idx: number, i: number][] = [];
 
-      if (/[^0-9.]/.test(top + lft + rgt + btm)) {
-        return +num[0];
+      for (let i = num.index - 1; i < num.index + num[0].length + 1; i++) {
+        adjacent.push([idx - 1, i]);
+        adjacent.push([idx + 1, i]);
+      }
+      adjacent.push([idx, num.index - 1]);
+      adjacent.push([idx, num.index + num[0].length + 1]);
+
+      const asterisk = adjacent.filter(([idx, i]) => arr[idx]?.[i] === "*");
+      if (asterisk) {
+        return {
+          asterisk,
+          number: +num[0],
+        };
       }
     })
   )
   .flat()
-  .reduce((acc, cur) => acc! + (cur ?? 0), 0);
+  .reduce((acc: Map<string, number[]>, cur) => {
+    cur?.asterisk.forEach(([idx, i]) => {
+      const key = `${idx},${i}`;
+      acc.set(key, [...(acc.get(key) ?? []), cur.number]);
+    });
+
+    return acc;
+  }, new Map());
 
 console.log(input);
+console.log(
+  [...input.values()].reduce((acc, cur) => {
+    if (cur.length === 2) {
+      const gear = cur[0] * cur[1];
+      return acc + gear;
+    }
+    return acc;
+  }, 0)
+);
