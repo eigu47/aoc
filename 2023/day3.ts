8eaf1f898c797edc3d1f4648232855bf7e01cbcf
Eiguchi Pablo <pablo.eiguchi@gmail.com>
2023-12-11 16:54:18 +0900

    day3-2!!!!!!!

--- 2023/day3.ts
+++ 2023/day3.ts
@@ -1,9 +1,11 @@
 import fs from "fs";
 
-const input = (fs.readFileSync("day3", "utf8") as string)
+const gears = new Map<string, number[]>();
+
+(fs.readFileSync("day3", "utf8") as string)
   .split("\n")
-  .map((line, idx, arr) =>
-    [...line.trim().matchAll(/(\d+)/g)].map((num) => {
+  .forEach((line, idx, arr) =>
+    [...line.trim().matchAll(/\d+/g)].forEach((num) => {
       if (num.index === undefined) return;
       const adjacent: [idx: number, i: number][] = [];
 
@@ -12,34 +14,21 @@ const input = (fs.readFileSync("day3", "utf8") as string)
         adjacent.push([idx + 1, i]);
       }
       adjacent.push([idx, num.index - 1]);
-      adjacent.push([idx, num.index + num[0].length + 1]);
+      adjacent.push([idx, num.index + num[0].length]);
 
-      const asterisk = adjacent.filter(([idx, i]) => arr[idx]?.[i] === "*");
-      if (asterisk) {
-        return {
-          asterisk,
-          number: +num[0],
-        };
-      }
+      adjacent
+        .filter(([idx, i]) => arr[idx]?.[i] === "*")
+        .forEach(([idx, i]) => {
+          const key = `${idx},${i}`;
+          const gearValue = gears.get(key) ?? [];
+          gearValue.push(+num[0]);
+          gears.set(key, gearValue);
+        });
     })
-  )
-  .flat()
-  .reduce((acc: Map<string, number[]>, cur) => {
-    cur?.asterisk.forEach(([idx, i]) => {
-      const key = `${idx},${i}`;
-      acc.set(key, [...(acc.get(key) ?? []), cur.number]);
-    });
+  );
 
-    return acc;
-  }, new Map());
-
-console.log(input);
 console.log(
-  [...input.values()].reduce((acc, cur) => {
-    if (cur.length === 2) {
-      const gear = cur[0] * cur[1];
-      return acc + gear;
-    }
-    return acc;
+  [...gears.values()].reduce((acc, cur) => {
+    return cur.length === 2 ? acc + cur[0] * cur[1] : acc;
   }, 0)
 );
