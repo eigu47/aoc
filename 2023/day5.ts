0e466cd919f95ff26518b98c57487d6ae0ec9883
Eiguchi Pablo <pablo.eiguchi@gmail.com>
2023-12-13 20:51:21 +0900

    day5-2

--- 2023/day5.ts
+++ 2023/day5.ts
@@ -11,26 +11,45 @@ const [seed, ...maps] = fs
       .map((str) => str.match(/\d+/g)?.map(Number) ?? [])
   );
 
-const newSeeds = seed[0].reduce((acc, num, idx, arr) => {
-  if (idx % 2 === 0) {
-    for (let i = num; i < num + arr[idx + 1]; i++) {
-      acc.push(i);
-    }
-  }
-  return acc;
-}, [] as number[]);
-
-const result = newSeeds.map((num) =>
-  maps.reduce((acc, map) => {
-    for (const [destination, origin, range] of map) {
-      if (acc >= origin && acc < origin + range) {
-        acc = acc - origin + destination;
-        break;
-      }
+const seedRanges = seed[0]
+  .reduce((acc, num, idx, arr) => {
+    if (idx % 2 === 0) {
+      acc.push([num, arr[idx + 1]]);
     }
     return acc;
-  }, num)
-);
+  }, [] as number[][])
+  .map(([start, range]) => {
+    const results: [destination: number, origin: number][] = [];
 
-console.log(Math.min(...result));
-console.log(newSeeds);
+    for (let i = start; i < start + range; ) {
+      let minRange = range;
+
+      const destination = maps.reduce((acc, map) => {
+        for (const [destination, origin, range] of map) {
+          if (acc >= origin && acc < origin + range) {
+            minRange = Math.min(minRange, origin + range - acc);
+            acc = acc - origin + destination;
+            break;
+          }
+        }
+        return acc;
+      }, i);
+
+      results.push([destination, i]);
+      i += minRange;
+    }
+
+    return results;
+  })
+  .flat()
+  .reduce(
+    (acc, result) => {
+      if (result[0] < acc[0]) {
+        acc = result;
+      }
+      return acc;
+    },
+    [Infinity, 0] as [destination: number, origin: number]
+  );
+
+console.log(seedRanges);
