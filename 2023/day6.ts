957e850022ee93a6c116d22a62cc7d2cff78d269
Eiguchi Pablo <pablo.eiguchi@gmail.com>
2023-12-13 22:56:30 +0900

    day6

---
+++ 2023/day6.ts
@@ -0,0 +1,21 @@
+import fs from "fs";
+
+const [times, distances] = fs
+  .readFileSync("day6", "utf8")
+  .split("\n")
+  .map((line) => line.split(":")[1].match(/\d+/g)?.map(Number) ?? []);
+
+const result = times.reduce((result, time, idx) => {
+  const distance = distances[idx];
+  let wins = 0;
+
+  for (let t = 1; t < time; t++) {
+    if (t * (time - t) > distance) {
+      wins++;
+    }
+  }
+
+  return result * wins;
+}, 1);
+
+console.log(result);
