36b84aa63882bab1aed9fa9df025b1d36be7e8c3
Eiguchi Pablo <pablo.eiguchi@gmail.com>
2023-12-16 19:23:14 +0900

    day7-2

--- 2023/day7.ts
+++ 2023/day7.ts
@@ -1,12 +1,9 @@
 import fs from "fs";
 
-type Hand = [cards: string, bid: number];
-
 const cardType = {
   A: 14,
   K: 13,
   Q: 12,
-  J: 11,
   T: 10,
   "9": 9,
   "8": 8,
@@ -16,6 +13,7 @@ const cardType = {
   "4": 4,
   "3": 3,
   "2": 2,
+  J: 1,
 };
 
 const handType = {
@@ -33,52 +31,62 @@ const input = fs
   .split("\r\n")
   .map((line) => {
     const [cards, bid] = line.split(" ");
-    return [cards, +bid] as Hand;
+
+    const repeated: Record<string, number> = {};
+    let j = 0;
+
+    for (let i = 0; i < cards.length; i++) {
+      const card = cards[i];
+      if (card === "J") {
+        j++;
+      } else if (cards.substring(i + 1).includes(card)) {
+        repeated[card] = (repeated[card] ?? 1) + 1;
+      }
+    }
+
+    let [high = 0, low] = Object.values(repeated).sort((a, b) => b - a);
+    high = high === 0 ? 1 + j : high + j;
+    let rank = 0;
+
+    if (high >= 5) {
+      rank = handType.five;
+    } else if (high === 4) {
+      rank = handType.four;
+    } else if (high === 3 && low === 2) {
+      rank = handType.full;
+    } else if (high === 3) {
+      rank = handType.three;
+    } else if (high === 2 && low === 2) {
+      rank = handType.two;
+    } else if (high === 2) {
+      rank = handType.one;
+    } else {
+      rank = handType.high;
+    }
+
+    return {
+      cards,
+      bid: +bid,
+      rank,
+    };
   })
   .sort((a, b) => {
-    const aRank = getRank(a[0]);
-    const bRank = getRank(b[0]);
+    const aRank = a.rank;
+    const bRank = b.rank;
 
     if (aRank !== bRank) return aRank - bRank;
 
-    for (let i = 0; i < a[0].length; i++) {
-      const aIdx = cardType[a[0][i] as keyof typeof cardType];
-      const bIdx = cardType[b[0][i] as keyof typeof cardType];
+    for (let i = 0; i < a.cards.length; i++) {
+      const aIdx = cardType[a.cards[i] as keyof typeof cardType];
+      const bIdx = cardType[b.cards[i] as keyof typeof cardType];
       if (aIdx !== bIdx) return aIdx - bIdx;
     }
+
     return 0;
   })
   .reduce((acc, cur, idx) => {
-    return acc + cur[1] * (idx + 1);
+    return acc + cur.bid * (idx + 1);
   }, 0);
 
-function getRank(cards: string) {
-  const repeated: Record<string, number> = {};
-
-  for (let i = 0; i < cards.length; i++) {
-    const card = cards[i];
-    if (cards.substring(i + 1).includes(card)) {
-      repeated[card] = (repeated[card] ?? 1) + 1;
-    }
-  }
-
-  const [high, low] = Object.values(repeated).sort((a, b) => b - a);
-
-  if (high === 5) {
-    return handType.five;
-  } else if (high === 4) {
-    return handType.four;
-  } else if (high === 3 && low === 2) {
-    return handType.full;
-  } else if (high === 3) {
-    return handType.three;
-  } else if (high === 2 && low === 2) {
-    return handType.two;
-  } else if (high === 2) {
-    return handType.one;
-  } else {
-    return handType.high;
-  }
-}
-
 console.log(input);
+// console.log(input.filter((hand) => hand.rank === 5));
