c075a5804180693f8e39918759649a27c81fb6e1
Eiguchi Pablo <pablo.eiguchi@gmail.com>
2023-12-18 13:10:24 +0900

    day8-2

--- 2023/day8.ts
+++ 2023/day8.ts
@@ -2,28 +2,43 @@ import fs from "fs";
 
 fs.readFile("day8", "utf8", (_, data) => {
   const [directions, ...rest] = data.split("\r");
+  const startNodes: string[] = [];
 
   const nodes = rest.reduce((acc, line) => {
     const [node, dir] = line.split("=").map((s) => s.match(/\w+/g));
     if (!node || !dir) return acc;
 
-    acc[node + ""] = {
+    acc[node[0]] = {
       L: dir[0],
       R: dir[1],
     };
 
+    if (node[0].at(-1) === "A") {
+      startNodes.push(node[0]);
+    }
+
     return acc;
   }, {} as Record<string, { L: string; R: string }>);
 
-  let current = "AAA";
-  let steps = 0;
+  const minSteps: number[] = [];
 
-  while (current !== "ZZZ") {
-    const node = nodes[current];
-    const dir = directions[steps % directions.length] as "L" | "R";
-    current = node[dir];
-    steps++;
-  }
+  startNodes.forEach((current) => {
+    let steps = 0;
 
-  console.log(steps);
+    while (current.at(-1) !== "Z") {
+      const node = nodes[current];
+      const dir = directions[steps % directions.length] as "L" | "R";
+      current = node[dir];
+      steps++;
+    }
+
+    minSteps.push(steps);
+  });
+
+  console.log(lcm(minSteps));
 });
+
+function lcm(nums: number[]) {
+  const gcd = (a: number, b: number): number => (b ? gcd(b, a % b) : a);
+  return nums.reduce((acc, num) => (acc * num) / gcd(acc, num));
+}
