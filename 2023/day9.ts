d298d75f84d6b3e8acbfc5c9d3aa4cbdb3266fc8
Eiguchi Pablo <pablo.eiguchi@gmail.com>
2023-12-18 13:56:02 +0900

    Refactor number difference calculation in day9.ts

--- 2023/day9.ts
+++ 2023/day9.ts
@@ -3,18 +3,22 @@ import fs from "fs";
 const input = fs
   .readFileSync("day9", "utf8")
   .split("\r\n")
-  .map((line) => line.split(" ")?.map(Number) ?? [])
+  .map((line) => line.split(" ").map(Number))
   .reduce((acc, nums) => {
-    const diffs: number[][] = [nums];
+    const diffs = [nums];
 
-    while (!diffs.at(-1)?.every((n) => n === 0)) {
+    while (true) {
       const cur = diffs.at(-1)!;
       const diff = cur.slice(0, -1).map((num, i) => cur[i + 1] - num);
 
       diffs.push(diff);
+
+      if (cur.every((n) => n === 0)) {
+        break;
+      }
     }
 
-    return acc + diffs.reduce((acc, cur) => acc + (cur.at(-1) ?? 0), 0);
+    return acc + diffs.reduce((acc, cur) => acc + cur.at(-1)!, 0);
   }, 0);
 
 console.log(input);
