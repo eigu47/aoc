41d0438a3c2b3c12f712c78dc8fa0fee3333ef00
Eiguchi Pablo <pablo.eiguchi@gmail.com>
2023-12-18 20:56:59 +0900

    day10-2

--- 2023/day10.ts
+++ 2023/day10.ts
@@ -1,8 +1,12 @@
 import fs from "fs";
 
-type Tile = [row: number, col: number];
+type Tile = [y: number, x: number];
 
 const pipesMap: Record<string, Tile[]> = {
+  S: [
+    [0, 1],
+    [1, 0],
+  ],
   "|": [
     [-1, 0],
     [1, 0],
@@ -29,7 +33,7 @@ const pipesMap: Record<string, Tile[]> = {
   ],
 };
 
-const input = fs.readFile("day10", "utf8", (error, data) => {
+fs.readFile("day10", "utf8", (_, data) => {
   const map = data.split("\r\n");
 
   const animal: Tile = [0, 0];
@@ -42,31 +46,49 @@ const input = fs.readFile("day10", "utf8", (error, data) => {
     }
   }
 
-  let cur: Tile = [animal[0], animal[1] + 1];
-  const visited = new Set([animal.toString(), cur.toString()]);
+  const path = new Set([animal.toString()]);
+  let cur = animal;
 
   while (true) {
     const pipe = map[cur[0]][cur[1]];
     const [dir1, dir2] = pipesMap[pipe];
 
     const next1: Tile = [cur[0] + dir1[0], cur[1] + dir1[1]];
-    if (!visited.has(next1.toString())) {
-      visited.add(next1.toString());
+    if (!path.has(next1.toString())) {
+      path.add(next1.toString());
       cur = next1;
       continue;
     }
 
     const next2: Tile = [cur[0] + dir2[0], cur[1] + dir2[1]];
-    if (!visited.has(next2.toString())) {
-      visited.add(next2.toString());
+    if (!path.has(next2.toString())) {
+      path.add(next2.toString());
       cur = next2;
       continue;
     }
 
-    if (map[next1[0]][next1[1]] === "S" || map[next2[0]][next2[1]] === "S") {
-      break;
-    }
+    // console.log(path.size / 2);
+    break;
   }
 
-  console.log(visited.size / 2);
+  const enclosed = map.reduce((acc, line, y) => {
+    let crosses = 0;
+
+    for (let x = 0; x < map[y].length; x++) {
+      if (path.has([y, x].toString())) {
+        if (line[x] === "|" || line[x] === "7" || line[x] === "F") {
+          crosses++;
+        }
+        continue;
+      }
+
+      if (crosses % 2 === 1) {
+        acc++;
+      }
+    }
+
+    return acc;
+  }, 0);
+
+  console.log(enclosed);
 });
