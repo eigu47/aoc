8524c84a79cd0a37375567da1e26f1a653b27a7f
Eiguchi Pablo <pablo.eiguchi@gmail.com>
2023-12-18 15:39:18 +0900

    day9-2

--- 2023/day9.ts
+++ 2023/day9.ts
@@ -7,18 +7,16 @@ const input = fs
   .reduce((acc, nums) => {
     const diffs = [nums];
 
-    while (true) {
+    while (!diffs.at(-1)!.every((n) => n === 0)) {
       const cur = diffs.at(-1)!;
       const diff = cur.slice(0, -1).map((num, i) => cur[i + 1] - num);
 
       diffs.push(diff);
-
-      if (cur.every((n) => n === 0)) {
-        break;
-      }
     }
 
-    return acc + diffs.reduce((acc, cur) => acc + cur.at(-1)!, 0);
+    const extrapolated = diffs.reverse().reduce((acc, cur) => cur[0] - acc, 0);
+
+    return acc + extrapolated;
   }, 0);
 
 console.log(input);
