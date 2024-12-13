9a353694244f925506eac94f8b6db3dd86350916
Eiguchi Pablo <pablo.eiguchi@gmail.com>
2024-12-13 19:02:46 +0900

    day 4 part 1

--- 2024/days/day4.go
+++ 2024/days/day4.go
@@ -14,28 +14,24 @@ var DIR = [8][2]int{
 	{0, 1},
 	{1, -1},
 	{1, 0},
-	{1, -1},
+	{1, 1},
 }
 
 func Day4_1() int {
 	input := util.GetInput(2024, 4)
 	res := 0
 
-	var checkXmas func(x, y, dir, char int) bool
-	checkXmas = func(x, y, dir, char int) bool {
-		if char >= len(CHAR) {
+	var checkXmas func(x, y, dir, idx int) bool
+	checkXmas = func(x, y, dir, idx int) bool {
+		if idx >= len(CHAR) {
 			return true
 		}
 
-		if x < 0 || x >= len(input) || y < 0 || y >= len(input[x]) {
+		if x < 0 || x >= len(input) || y < 0 || y >= len(input[x]) || input[x][y] != CHAR[idx] {
 			return false
 		}
 
-		if input[x][y] == CHAR[char] {
-			return checkXmas(x+DIR[dir][0], y+DIR[dir][1], dir, char+1)
-		}
-
-		return false
+		return checkXmas(x+DIR[dir][0], y+DIR[dir][1], dir, idx+1)
 	}
 
 	for x, row := range input {
