546f0b3c2b58dbbed0e9a6b8e3668ac6a94fa299
Martin Eiguchi <martin.eiguchi.bp@nttdata.com>
2024-12-16 15:56:21 +0900

    day 5 part 2

--- 2024/days/day5.go
+++ 2024/days/day5.go
@@ -2,6 +2,7 @@ package days
 
 import (
 	"slices"
+	"sort"
 	"strconv"
 	"strings"
 
@@ -47,6 +48,56 @@ func Day5_1() int {
 		}
 
 		if isValid {
+			res += update[len(update)/2]
+		}
+	}
+
+	return res
+}
+
+func Day5_2() int {
+	var input = util.GetInput(2024, 5)
+
+	updates := [][]int{}
+	rules := make(map[int][]int)
+	res := 0
+
+	isRules := true
+	for _, line := range input {
+		if line == "" {
+			isRules = false
+			continue
+		}
+
+		if isRules {
+			pages := strings.Split(line, "|")
+			rgt, _ := strconv.Atoi(pages[0])
+			lft, _ := strconv.Atoi(pages[1])
+			rules[rgt] = append(rules[rgt], lft)
+		} else {
+			var update []int
+			for _, u := range strings.Split(line, ",") {
+				num, _ := strconv.Atoi(u)
+				update = append(update, num)
+			}
+			updates = append(updates, update)
+		}
+	}
+
+	for _, update := range updates {
+		isValid := true
+		for i := 0; i+1 < len(update); i++ {
+			if !slices.Contains(rules[update[i]], update[i+1]) {
+				sort.Slice(update, func(i, j int) bool {
+					return slices.Contains(rules[update[i]], update[j])
+				})
+				isValid = false
+
+				break
+			}
+		}
+
+		if !isValid {
 			res += update[len(update)/2]
 		}
 	}
