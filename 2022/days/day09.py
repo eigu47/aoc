a8847334397b43a4ec41f1f9af8e285459e76adb
Eiguchi Pablo <pablo.eiguchi@gmail.com>
2026-01-11 08:51:50 +0900

    better part1

--- 2022/days/day09.py
+++ 2022/days/day09.py
@@ -2,35 +2,25 @@ def day_09_1(input: str) -> int:
     ans = 0
     head = [0, 0]
     tail = [0, 0]
-    prev = [0, 0]
     visited = {",".join(map(str, head))}
-    path = []
 
     for line in input.splitlines():
         dir, steps = line.split()
-        # print(dir, steps)
         for _ in range(int(steps)):
             head = [
                 head[0] + dirs[dir][0],
                 head[1] + dirs[dir][1],
             ]
-            for d in dirs.values():
-                if tail[0] == head[0] + d[0] and tail[1] == head[1] + d[1]:
-                    x = prev[0] + dirs[dir][0]
-                    y = prev[1] + dirs[dir][1]
-                    prev = [(x > 0) - (x < 0), (y > 0) - (y < 0)]
-                    break
-            else:
-                tail[0], tail[1] = (
-                    tail[0] + prev[0],
-                    tail[1] + prev[1],
-                )
-                visited.add(",".join(map(str, tail)))
-                # path.append(",".join(map(str, tail)))
-                prev = dirs[dir]
-            # print(head, tail)
+            dy = head[0] - tail[0]
+            dx = head[1] - tail[1]
+            if abs(dy) > 1 or abs(dx) > 1:
+                tail = [
+                    tail[0] + ((dy > 0) - (dy < 0)),
+                    tail[1] + ((dx > 0) - (dx < 0)),
+                ]
 
-    # print(path)
+            # print(head, tail)
+            visited.add(",".join(map(str, tail)))
 
     ans = len(visited)
     return ans
@@ -43,7 +33,6 @@ def day_09_2(input: str) -> int:
 
 
 dirs = {
-    "I": [0, 0],
     "U": [-1, 0],
     "R": [0, 1],
     "D": [1, 0],
